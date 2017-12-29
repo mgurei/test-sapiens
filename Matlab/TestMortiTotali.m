2708cf9255bdd0b44fe630f56cca699c67e8873a
Serbitar31 <saksida.marco@hotmail.it>
2017-12-29 21:50:57 +0100

    funziona_meglio

--- Matlab/TestMortiTotali.m
+++ Matlab/TestMortiTotali.m
@@ -1,25 +1,26 @@
 %Test Computo totale dei cadaveri
-Urtati = zeros(30,5);
-Legnati = zeros (30, 5);
-MortiAmmazzati = zeros(30,5);
 
-for F = 1:5
-for n = 1:30
-Urtati(n,F) = MortiUrto2(3, F, 2, 0, 0, 50, 50,.5, .5, .6, .6,...
-    300);
+clear all; close all; clc;
 
+F_ratio = 0.2:.1:5;
 
-for Danno = 1:30
+Urtati = zeros(1,length(F_ratio));
+Legnati = zeros(30,length(F_ratio));
+MortiAmmazzati = zeros(30,length(F_ratio));
+
+for i = 1:length(F_ratio)
+    F = F_ratio(i);
+    for danno = 1:30
+    Urtati(i) = MortiUrto2(3, F, 2, 0, 0, 50, 50,.5, .5, .6, .6,...
+        300);
+    Legnati(danno,i) = MortiArma2(.5, .5,F,danno,300, 50 );
     
-
-        
-Legnati(Danno,F) = MortiArma2(.5, .5,F,Danno,300, 50 );
-MortiAmmazzati = (Urtati + Legnati);
-end
-end
+    MortiAmmazzati(danno, i) = Urtati(i) + Legnati(danno, i);
+    end
 end
 
 
-
-%% plot
-plot(MortiAmmazzati)
+% plot
+plot(F_ratio, MortiAmmazzati)
+xlabel('F1/F2')
+ylabel('Morti')
