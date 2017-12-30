2ec6cf22acf3bed45b24d4cfdc8e4c6c26caef83
Mihai Gurei <gurei.mihai@gmail.com>
2017-12-30 18:25:52 +0100

    Add master file

--- Matlab/master.m
+++ Matlab/master.m
@@ -1,8 +1,20 @@
+% @Author: Mihai Gurei <mihaig>
+% @Date:   "Saturday, 30th December 2017, 18:15:22 pm"
+% @Email:  mihai.gurei@analphabeta.com
+% @Filename: master.m
+% @License: lgpl3
+
 %% This file is the master configuration file for the full and correct
 % set up of the matlab testing and simulation environment.
 
-% Make all subfolders part of the working directory
-addpath('combat')
-addpath('items')
-addpath('test')
-savepath pathdef.m
+% Make all subfolders part of the working directory, so the files inside
+% can be used
+addpath(genpath('combat'))  % Add combat functions
+addpath(genpath('items'))    % Add items specifics
+addpath(genpath('test'))     % Add test functions
+
+
+%%
+
+
+%EOF
