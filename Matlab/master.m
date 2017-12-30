f85708d5f5c901d90dad6b336d2e2f1ea46397d4
Mihai Gurei <gurei.mihai@gmail.com>
2017-12-30 15:50:48 +0100

    add path definition

---
+++ Matlab/master.m
@@ -0,0 +1,8 @@
+%% This file is the master configuration file for the full and correct
+% set up of the matlab testing and simulation environment.
+
+% Make all subfolders part of the working directory
+addpath('combat')
+addpath('items')
+addpath('test')
+savepath pathdef.m
