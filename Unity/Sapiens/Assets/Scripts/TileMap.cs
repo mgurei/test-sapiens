d7edae14b2f557ed85c83afc0e220e5fcd4baba5
mgurei <gurei.mihai@gmail.com>
2018-01-04 14:33:35 +0100

    Init Unity project

---
+++ Unity/Sapiens/Assets/Scripts/TileMap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Required components
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshCollider))]
+
+public class TileMap : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+}
