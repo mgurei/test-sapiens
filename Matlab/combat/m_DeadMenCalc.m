ca240babc9868974cfe73b8dd2276d03096eb0e4
Mihai Gurei <gurei.mihai@gmail.com>
2018-01-12 04:48:17 +0100

    Hex made of squares

--- Matlab/combat/m_DeadMenCalc.m
+++ Matlab/combat/m_DeadMenCalc.m
@@ -12,13 +12,14 @@
 % - Damage calculated from pure damage / armor reduction.
 % - Numbers and HP modifier.
 
-function deadmen = m_DeadMenCalc(pure_damage, armor, paramA, paramD)
+function deadmen = m_DeadMenCalc(pure_damage, armor, paramA, paramD, charge)
     % Attacker
     % pure_damage(1:3): cut, blunt, pierce
     % paramA: to be defined
     % Defenser
     % armor(1:3): cur, blunt, pierce
     % patamD: to be defined
+    % charge: [bool] indicates if attacker is charging
 
     % Experience
     % TODO: give an actual attrubution from param values
@@ -38,8 +39,18 @@ function deadmen = m_DeadMenCalc(pure_damage, armor, paramA, paramD)
     hpD = 50;
     numRatio = m_Ratio(num, hpD);
 
-    deadmen = formMod * expRatio * damDealt * numRatio;
+    deadmen_fight = formMod * expRatio * damDealt * numRatio;
 
+    % Calculation of charge damage
+    if (charge)
+        deadmen_charge + formMod * 1;  % TODO: Finish formula
+    end
+
+    deadmen = deadmen_fight + deadmen_charge;
+    fprintf('Result:\n');
+    fprintf('Total dead: %d\n', deadmen);
+    fprintf('Dead in charge: %d', deadmen_charge);
+    fprintf('Deadman in fight: %d', deadmen_charge);
     % Charge damahe
     charge = false;     % TODO: get charge value from somewhere
     if (charge)
